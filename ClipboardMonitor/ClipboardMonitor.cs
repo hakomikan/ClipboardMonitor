7bb5c1e0ac3dccf6ba311c34ef1db829940ad302
hakomikan <mail@s-taki.sakura.ne.jp>
2015-08-17 22:55:34 +0900

    add first version library and a test.

---
+++ ClipboardMonitor/ClipboardMonitor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace ClipboardMonitor
+{
+    public delegate void ReceiveClipboardEventHandler();
+
+    public class ClipboardMonitor : NativeWindow
+    {
+        [DllImport("user32")]
+        public static extern IntPtr SetClipboardViewer(
+                IntPtr hWndNewViewer);
+
+        [DllImport("user32")]
+        public static extern bool ChangeClipboardChain(
+                IntPtr hWndRemove, IntPtr hWndNewNext);
+
+        [DllImport("user32")]
+        public extern static int SendMessage(
+                IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+        private const int WM_DRAWCLIPBOARD = 0x0308;
+        private const int WM_CHANGECBCHAIN = 0x030D;
+        private IntPtr HWND_MESSAGE = (IntPtr) (-3);
+        private IntPtr nextHandle;
+
+        public event ReceiveClipboardEventHandler ClipboardReceived;
+
+        public ClipboardMonitor()
+        {
+            this.CreateHandle(new CreateParams
+            {
+                Parent = HWND_MESSAGE
+            });
+
+            nextHandle = SetClipboardViewer(this.Handle);
+        }
+
+        protected override void WndProc(ref Message msg)
+        {
+            switch (msg.Msg)
+            {
+                case WM_DRAWCLIPBOARD:
+                    if (Clipboard.ContainsText())
+                    {
+                        ClipboardReceived();
+                    }
+                    if ((int)nextHandle != 0)
+                        SendMessage(
+                            nextHandle, msg.Msg, msg.WParam, msg.LParam);
+                    break;
+
+                case WM_CHANGECBCHAIN:
+                    if (msg.WParam == nextHandle)
+                    {
+                        nextHandle = (IntPtr)msg.LParam;
+                    }
+                    else if ((int)nextHandle != 0)
+                        SendMessage(
+                            nextHandle, msg.Msg, msg.WParam, msg.LParam);
+                    break;
+            }
+            base.WndProc(ref msg);
+        }
+    }
+}
---
+++ ClipboardMonitorTest/ClipboardMonitorTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClipboardMonitor;
+using System.Windows.Forms;
+
+namespace ClipboardMonitorTest
+{
+    [TestClass]
+    public class ClipboardMonitorTest
+    {
+        [TestMethod]
+        public void TestBasic()
+        {
+            var viewer = new ClipboardMonitor.ClipboardMonitor();
+            IDataObject content = null;
+
+            viewer.ClipboardReceived += () => content = Clipboard.GetDataObject();
+
+            Assert.AreEqual(null, content);
+            Clipboard.SetText("test");
+            Assert.AreNotEqual(null, content);
+        }
+    }
+}
